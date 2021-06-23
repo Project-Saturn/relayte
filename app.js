421df47c0ac9d45d39f66db99f4675f92bc50cd9
Anthony Palumbo <anthonypalumbo82@gmail.com>
2021-06-23 10:52:22 +0900

    Added error logging to API endpoints.

--- app.js
+++ app.js
@@ -12,93 +12,153 @@ app.use(express.json());
 app.use(express.static('build'));
 
 app.get('/api/reservations' , async (req, res) => {
-  console.log('Get all reservations');
-  const response = await knex('reservations').select('*');
-  console.log(response);
-  res.json(response);
+  try {
+    console.log('Get all reservations');
+    const response = await knex('reservations').select('*');
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.get('/api/customers' , async (req, res) => {
-  console.log('Get all customers');
-  const response = await knex('customers').select('*');
-  console.log(response);
-  res.json(response);
+  try {
+    console.log('Get all customers');
+    const response = await knex('customers').select('*');
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.get('/api/translators' , async (req, res) => {
-  console.log('Get all translators');
-  const response = await knex('translators').select('*');
-  console.log(response);
-  res.json(response);
+  try {
+    console.log('Get all translators');
+    const response = await knex('translators').select('*');
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.get('/api/reservations/:id' , async (req, res) => {
-  console.log(`Get reservation ${req.params.id}`);
-  const response = await knex('reservations').select('*').where('id', req.params.id);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log(`Get reservation ${req.params.id}`);
+    const response = await knex('reservations').select('*').where('id', req.params.id);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.get('/api/customers/:id' , async (req, res) => {
-  console.log(`Get customer ${req.params.id}`);
-  const response = await knex('customers').select('*').where('id', req.params.id);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log(`Get customer ${req.params.id}`);
+    const response = await knex('customers').select('*').where('id', req.params.id);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.get('/api/translators/:id' , async (req, res) => {
-  console.log(`Get translator ${req.params.id}`);
-  const response = await knex('translators').select('*').where('id', req.params.id);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log(`Get translator ${req.params.id}`);
+    const response = await knex('translators').select('*').where('id', req.params.id);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.post('/api/reservations' , async (req, res) => {
-  console.log('Create New Reservation');
-  console.log(req.body.reservation);
-  const response = await knex('reservations').insert(req.body.reservation);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log('Create New Reservation');
+    console.log(req.body.reservation);
+    const response = await knex('reservations').insert(req.body.reservation);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.post('/api/customers' , async (req, res) => {
-  console.log('Create New Customer');
-  console.log(req.body.customer);
-  const response = await knex('customers').insert(req.body.customer);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log('Create New Customer');
+    console.log(req.body.customer);
+    const response = await knex('customers').insert(req.body.customer);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.post('/api/translators' , async (req, res) => {
-  console.log('Create New Translator');
-  console.log(req.body.translator);
-  const response = await knex('translators').insert(req.body.translator);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log('Create New Translator');
+    console.log(req.body.translator);
+    const response = await knex('translators').insert(req.body.translator);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.put('/api/reservations/:id' , async (req, res) => {
-  console.log(`Edit reservation id ${req.params.id}`);
-  console.log(req.body.reservation);
-  const response = await knex('reservations').where('id', req.params.id).update(req.body.reservation);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log(`Edit reservation id ${req.params.id}`);
+    console.log(req.body.reservation);
+    const response = await knex('reservations').where('id', req.params.id).update(req.body.reservation);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.put('/api/customers/:id' , async (req, res) => {
-  console.log(`Edit customer id ${req.params.id}`);
-  console.log(req.body.customer);
-  const response = await knex('customers').where('id', req.params.id).update(req.body.customer);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log(`Edit customer id ${req.params.id}`);
+    console.log(req.body.customer);
+    const response = await knex('customers').where('id', req.params.id).update(req.body.customer);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.put('/api/translators/:id' , async (req, res) => {
-  console.log(`Edit translator id ${req.params.id}`);
-  console.log(req.body.translator);
-  const response = await knex('translators').where('id', req.params.id).update(req.body.translator);
-  console.log(response);
-  res.json(response);
+  try {
+    console.log(`Edit translator id ${req.params.id}`);
+    console.log(req.body.translator);
+    const response = await knex('translators').where('id', req.params.id).update(req.body.translator);
+    console.log(response);
+    res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json(error.message);
+  }
 });
 
 app.get('/room', async (req, res) => {
